d7a28182431f335ee119035aba6ea97909aeb272
Ammyy9908 <sb78639@gmail.com>
2021-08-25 11:52:05 +0530

    dynamic album added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -344,7 +344,7 @@ props.uid && fetchUser().then((user)=>{
   const handleAlbumPlay = async ()=>{
         try{
             const r = await axios.put('https://api.spotify.com/v1/me/player/play?device_id=ec896299ed25778bb88c6091cd2562e0eedb2b20',{
-                "context_uri": "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr",
+                "context_uri": `spotify:album:${playlist.id}`,
                 "offset": {
                   "position": 5
                 },
