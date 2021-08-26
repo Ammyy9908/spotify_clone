85a867a5ba0f0c2bc2f6203a1e9dc5c6fda88276
Ammyy9908 <sb78639@gmail.com>
2021-08-26 22:25:44 +0530

    greeting fixed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -188,11 +188,13 @@ function Recommendation({recommendations,handlePlay}){
 
             <div className="recommendation__section">
                 <div className="recommendation__header">
-                    <h2>Good {new Date().getHours()<12 && "morning 🙏"} {new Date().getHours()>=12 || new Date().getHours()<16 ? "afternoon 😎":null} {new Date().getHours()>16 || new Date().getHours()<20 ? "evening 🌦":null} {new Date().getHours()>20 ?  "night 😴":null} </h2>
+                    <h2>Good {new Date().getHours()<12 ? "morning 🙏":null} 
+                    {new Date().getHours()>=12 || new Date().getHours()<17 ? "afternoon 😎":null}
+                     {new Date().getHours()>17 || new Date().getHours()<20 ? "evening 🌦":null}
+                      {new Date().getHours()>20 ?  "night 😴":null} </h2>
                 </div>
                 <div className="recommendation__grid">
-                   {/* <RecommendationCard/>
-                   <RecommendationCard/> */}
+               
                    
 
                    {
